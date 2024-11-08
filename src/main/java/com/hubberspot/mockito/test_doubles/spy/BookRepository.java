c4416d0eabaab77e43d22d2870cfe53cd46ebcfa
shisir <shisirhumagain0@gmail.com>
2024-11-07 23:10:26 -0500

    spy files added

---
+++ src/main/java/com/hubberspot/mockito/test_doubles/spy/BookRepository.java
@@ -0,0 +1,7 @@
+package com.hubberspot.mockito.test_doubles.spy;
+
+
+public interface BookRepository {
+
+    void save(Book book);
+}
---
+++ src/test/java/com/hubberspot/mockito/test_doubles/spy/SpyTest.java
@@ -0,0 +1,28 @@
+package com.hubberspot.mockito.test_doubles.spy;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SpyTest {
+
+    @Test
+    public void demoSpy(){
+        BookRepositorySpy bookRepositorySpy = new BookRepositorySpy();
+        BookService bookService = new BookService(bookRepositorySpy);
+
+        Book book1 = new Book("1234", "Mockito in Action", 500, LocalDate.now());
+        Book book2 = new Book("1235", "JUnit in Action", 400, LocalDate.now());
+
+        bookService.addBook(book1);
+        assertEquals(0, bookRepositorySpy.timesCalled());
+
+        bookService.addBook(book2);
+        assertEquals(1, bookRepositorySpy.timesCalled());
+
+        //assertTrue(bookRepositorySpy.calledWith(book2));
+    }
+}
