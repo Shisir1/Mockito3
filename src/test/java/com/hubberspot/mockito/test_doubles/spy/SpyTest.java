7d1fa06d1b15853c2c8c47b69f041ac208013841
shisir <shisirhumagain0@gmail.com>
2024-11-07 23:13:11 -0500

    modification in SpyTest

--- src/test/java/com/hubberspot/mockito/test_doubles/spy/SpyTest.java
+++ src/test/java/com/hubberspot/mockito/test_doubles/spy/SpyTest.java
@@ -9,6 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SpyTest {
 
+    // Spy is similar to Stub but it also has additional feature
+    //of recording the events like external calls and all, in this
+    //example, we are simply saving the book in the repository but
+    // we can't really tell whether book was actually added or not
+    //so, in this case we can use spy to track the calls and test its
+    //behavior.
     @Test
     public void demoSpy(){
         BookRepositorySpy bookRepositorySpy = new BookRepositorySpy();
